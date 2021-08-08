f808ceb12ba021aa0e9db8c0acede25208d17b41
Jovennan Ramalho <jovennan@gmail.com>
2021-08-07 22:55:12 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screens.dart';
+import 'screens/tabs_screen.dart';
+import 'screens/meal_detail_screen.dart';
+import 'utils/app_routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DeliMeals',
+      theme: ThemeData(
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
+      // home: CategoriesScreen(),
+      routes: {
+        AppRoutes.HOME: (ctx) => TabsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        // AppRoutes.FAVORITES_MEALS: (ctx) => FavoriteScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/alguma-coisa') {
+      //     return null;
+      //   } else if (settings.name == '/outra-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(builder: (_) {
+      //       return CategoriesScreen();
+      //     });
+      //   }
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (_) {
+            return CategoriesScreen();
+          },
+        );
+      },
+    );
+  }
+}
